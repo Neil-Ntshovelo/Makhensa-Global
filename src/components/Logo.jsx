70321d72f50813ac1c0ca15519f98f928a40278f
Ntshovelo Nell <neilmunetwa@gmail.com>
2025-04-24 11:29:38 +0200

    Add files via upload

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import logo from '../assets/logo.jpg'
+import React from 'react';
+import logo from '../assets/logo.jpg'; 
 
 const Logo = () => {
   return (
-    <div className='logo h-16 w-20'>
-        <img src={logo} alt='logo' className='rounded-2xl ' />
+    <div className='logo h-24 w-28'>
+      <img src={logo} alt='logo' className='rounded-2xl h-22 w-32' />
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
